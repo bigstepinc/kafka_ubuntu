63053826fc5e2ff5f3190beb5f608d4c67845f5e
cristina-grosu <cristina.grosu@bigstep.com>
2017-08-03 04:50:26 +0300

    Update init-docker.sh

--- init-docker.sh
+++ init-docker.sh
@@ -3,6 +3,14 @@
 # Prepare the environment
 apt-get update && apt-get -y install wget tar curl curl git unzip dnsutils  net-tools && apt-get clean 
 
+# Install Java 8
+cd /opt && wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz" &&\
+tar xzf jdk-8u141-linux-x64.tar.gz && rm -rf jdk-8u141-linux-x64.tar.gz
+
+export JAVA_HOME=/opt/jdk1.8.0_141
+export PATH=$PATH:/opt/jdk1.8.0_141/bin:/opt/jdk1.8.0_141/jre/bin:/etc/alternatives:/var/lib/dpkg/alternatives
+
+
 # Application download and install
 cd /opt && wget http://apache.mirror.anlx.net/kafka/0.10.1.0/kafka_2.11-0.10.1.0.tgz 
 cd /opt && tar xzvf /opt/kafka_2.11-0.10.1.0.tgz 
